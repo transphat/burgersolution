61ef62d4a1fc3e817dd25edb42592886c99e075b
pasipiya <piyathilaka10@gmail.com>
2021-01-03 01:58:54 +0530

    configure heroku db creditials

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "ec2-34-194-198-238.compute-1.amazonaws.com",
+  port: 5432,
+  user: "luswetbexetmyn",
+  password: "d65c67f1e0592a9dd9379bfdfc8de80d00a47eaf690997955a6a7954ac59e083",
+  database: "d597v9tdufv33u"
 });
 
 
